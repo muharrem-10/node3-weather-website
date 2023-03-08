381d2df70e37adae976fec41c735a504a4c67acd
muharrem bahçeci <muharrembahceci@muharrem.local>
2023-03-08 11:04:08 +0300

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude,longitude, callback) => {
+    const url='http://api.weatherstack.com/current?access_key=83b71e98aa92d04a4777ccefb35ced57&query='+latitude+','+longitude
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {       //koordinatları sildiğimde aldığım hata (-75.7088, 44.1545)
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            temperature= body.current.temperature,
+            fTemperature= body.current.feelslike,
+            description= body.current.weather_descriptions[0]
+            callback(undefined, "The weather is "+description+".it is currently "+temperature+" degrees out.İt feels like "+fTemperature+" degrees out")
+        }
+    })
+}
+
+module.exports = forecast
